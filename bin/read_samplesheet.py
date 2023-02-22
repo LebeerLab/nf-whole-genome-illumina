b056328e5753bfa9d4e80e7f02d047ef8b6ab96c
TheOaphidian <tim.van.rillaer@hotmail.com>
2023-02-22 18:44:09 +0100

    Fix uqid recognition and assembly paths

--- bin/read_samplesheet.py
+++ bin/read_samplesheet.py
@@ -3,6 +3,7 @@
 import os
 import argparse
 from pathlib import Path
+import re
 import pandas as pd
 
 from utils import generate_uqid
@@ -12,6 +13,7 @@ DEF_SAMPLE_ID = "ID"
 DEF_FW_READS = "fw_reads"
 DEF_RV_READS = "rv_reads"
 DEF_ASSEMBLY = "assembly"
+DEF_RUN = "run01"
 
 
 class SampleSheet:
@@ -24,6 +26,12 @@ class SampleSheet:
         sample_db_dir=None,
         run_id=None,
     ) -> None:
+        # Validation input
+        if sample_col == None: sample_col = DEF_SAMPLE_ID
+        if fw_col == None: fw_col = DEF_FW_READS
+        if rev_col == None: rev_col = DEF_RV_READS
+        if run_id == None: run_id = DEF_RUN
+
         self.sample_col = sample_col
         self.fw_col = fw_col
         self.rev_col = rev_col
@@ -54,11 +62,16 @@ class SampleSheet:
         self.content = smpsh
 
     def _build_read_paths(self, absolute=False):
-        # Update paths to absolute paths.
-        self.content[[self.fw_col, self.rev_col, DEF_ASSEMBLY]] = self.content[
-            [self.fw_col, self.rev_col, DEF_ASSEMBLY]
+        # Update paths to rel paths.
+        self.content[[self.fw_col, self.rev_col]] = self.content[
+            [self.fw_col, self.rev_col]
         ].apply(lambda x: self._fetch_filepath(x, absolute))
 
+    def _build_assembly_paths(self):
+        # Update paths to absolute paths.
+        self.content[[DEF_ASSEMBLY]] = self.content[[DEF_ASSEMBLY]].apply(
+            lambda x: self._fetch_filepath(x, absolute=True, is_assembly=True)
+        )
 
     def update_samplesheet(self):
 
@@ -67,8 +80,7 @@ class SampleSheet:
         self.content["run_id"] = self.run_id
         # Generate uqid
         self.content["uqid"] = (
-            self.content[["run_id", self.fw_col, self.rev_col]]
-            .sum(axis=1)
+            self.content[["run_id", self.fw_col, self.rev_col]].astype(str).agg("".join, axis=1)
             .map(generate_uqid)
         )
 
@@ -83,7 +95,7 @@ class SampleSheet:
 
         # Add resulting assembly path as column
         # RESULTS / ID / RUN / ASSEMBLY / ID_contigs.fna
-        root_contig = os.path.dirname(self.filename).split("/data")[0] + "/results/"
+        root_contig = self.root_dir + "/results/"
         self.content[DEF_ASSEMBLY] = [
             f"{root_contig}{id}/{run}/assembly/{id}_contigs.fna"
             for id, run in zip(self.content[DEF_SAMPLE_ID], self.content["run_id"])
@@ -93,44 +105,45 @@ class SampleSheet:
         self.content.to_csv(CORR_SAMPLESHEET, sep="\t", index=False)
 
     def update_sampledb(self):
-        # Add absolute root
+        # Add absolute root to reads and assembly
         self._build_read_paths(absolute=True)
-
+        self._build_assembly_paths()
+        output = self.content
+        
         if Path(self.sample_db_samplesheet).exists():
             db_content = pd.read_table(self.sample_db_samplesheet)
-            db_content = db_content.append(
-                self.content[~self.content[DEF_SAMPLE_ID].isin(db_content[DEF_SAMPLE_ID])],
-                ignore_index=True,
-            )
+            output = pd.concat([db_content,
+                self.content[
+                    ~self.content["uqid"].isin(db_content["uqid"])
+                ]
+            ])
+        
+        output.to_csv(self.sample_db_samplesheet, sep="\t", index=False)
 
-            db_content.to_csv(self.sample_db_samplesheet, sep="\t", index=False)
-        else:
-            self.content.to_csv(self.sample_db_samplesheet, sep="\t", index=False)
-
-    def _fetch_filepath(self, read_files, absolute=False):
+    def _fetch_filepath(self, inp_files, absolute=False, is_assembly=False):
         def simplify_samplenames(filenames: pd.Series) -> pd.Series:
             return filenames.apply(
                 lambda x: x.lower().split(".")[0].split("/")[-1].strip()
             )
 
         def simplify_samplename(filename: str) -> str:
-            return filename.lower().split(".")[0].strip() 
+            return filename.lower().split(".")[0].strip()
 
         def find_matching_file(filename, rootdir, absolute=False):
             for root, _, files in os.walk(rootdir):
                 for file in files:
-                    if simplify_samplename(file) == simplify_samplename(filename):
+                    if simplify_samplename(file) == filename:
                         if absolute:
-                            absolute_root = os.path.join(self.root_dir, root)
-                            return os.path.join(absolute_root, file)    
+                            root = os.path.join(self.root_dir, root)
                         return os.path.join(root, file)
             raise FileNotFoundError(
                 f"Could not locate the file specified as {filename}"
             )
+        if is_assembly: rt = os.path.join(self.root_dir, "results")
+        else: rt = os.path.dirname(self.filename)
+        simple_names = simplify_samplenames(inp_files)
 
-        smpsh_dir = os.path.dirname(self.filename)
-        reads_smp = simplify_samplenames(read_files)
-        return reads_smp.apply(lambda x: find_matching_file(x, smpsh_dir, absolute))
+        return simple_names.apply(lambda x: find_matching_file(x, rt, absolute))
 
 
 if __name__ == "__main__":
