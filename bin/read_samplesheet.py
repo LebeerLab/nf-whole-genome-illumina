45e3e7218305a4dd262b32823ac9df8694762adc
TheOaphidian <tim.van.rillaer@hotmail.com>
2023-02-27 13:33:36 +0100

    Move samplesheet correction to read module

--- bin/read_samplesheet.py
+++ bin/read_samplesheet.py
@@ -65,6 +65,7 @@ class ReadSampleSheet(object):
             args.run_name,
         )
         smpsh.read_samplesheet()
+        smpsh.update_samplesheet()
         smpsh.write_samplesheet()
 
     def write(self):
@@ -82,7 +83,6 @@ class ReadSampleSheet(object):
             args.samplesheet, DEF_SAMPLE_ID, DEF_FW_READS, DEF_RV_READS, None, None
         )
         smpsh.read_samplesheet()
-        smpsh.update_samplesheet()
         smpsh.update_sampledb()
 
 
